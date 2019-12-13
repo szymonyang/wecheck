3f5650f5f9f98f0be8173f03efdfc8517581dab3
Andrew Houghton <houghtonandrew0@gmail.com>
2019-12-14 10:49:00 +1100

    Skeleton code without consumer code

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# mysite/settings.py
+# Channels
+ASGI_APPLICATION = 'mysite.routing.application'
