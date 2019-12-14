b0417fc4bd911ff0b52eb8eeba0c0a2da2e273d5
Andrew Houghton <houghtonandrew0@gmail.com>
2019-12-14 15:04:27 +1100

    Add models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    channel = models.CharField(max_length=100, unique=True)
+    status = models.TextChoices('queue', 'chatting')
+
+
+class Doctor(models.Model):
+    channel = models.CharField(max_length=100, unique=True)
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.SET_NULL)
